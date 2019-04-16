77d659ae14870def2ee410c6c403c7cc18321944
robert <rob@collaborizm.com>
2019-04-16 16:33:35 -0400

    ignore

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+function setCookie(cname: string, cvalue: string, exdays: number) {
+    const d = new Date()
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+    const expires = d.toUTCString()
+    const cookie = `${cname}=${cvalue}; expires=${expires}; path=/;`
+    document.cookie = cookie
+}
+
+function getCookie(cname: string, cookieHeader: string) {
+    const m: Record<string, string> = {}
+    cookieHeader.split(';').map(item => item.trim().split('=')).forEach(item => m[item[0]] = item[1])
+    const value = m[cname]
+
+    if (!value) {
+        return null
+    }
+
+    if (value.trim().length === 0) {
+        return null
+    }
+
+    return value
+}
+
+function deleteCookie(cname: string) {
+    const d = new Date()
+    d.setTime(0)
+    const expires = d.toUTCString()
+    const cookie = `${cname}=; expires=${expires}; path=/;`
+    document.cookie = cookie
+}
+
+export { setCookie, getCookie, deleteCookie }
