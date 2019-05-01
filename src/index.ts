49588fc4f3b91cf0d9717cbdca4f19953b7a90d7
Robert Lancer <rob@collaborizm.com>
2019-05-01 17:54:42 -0400

    null check on cookie header

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,40 @@
 function setCookie(cname: string, cvalue: string, exdays: number) {
-    const d = new Date()
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
-    const expires = d.toUTCString()
-    const cookie = `${cname}=${cvalue}; expires=${expires}; path=/;`
-    document.cookie = cookie
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  const expires = d.toUTCString();
+  const cookie = `${cname}=${cvalue}; expires=${expires}; path=/;`;
+  document.cookie = cookie;
 }
 
 function getCookie(cname: string, cookieHeader: string) {
-    const m: Record<string, string> = {}
-    cookieHeader.split(';').map(item => item.trim().split('=')).forEach(item => m[item[0]] = item[1])
-    const value = m[cname]
+  const m: Record<string, string> = {};
 
-    if (!value) {
-        return null
-    }
+  if (!cookieHeader) {
+    return null;
+  }
+  cookieHeader
+    .split(";")
+    .map(item => item.trim().split("="))
+    .forEach(item => (m[item[0]] = item[1]));
+  const value = m[cname];
 
-    if (value.trim().length === 0) {
-        return null
-    }
+  if (!value) {
+    return null;
+  }
 
-    return value
+  if (value.trim().length === 0) {
+    return null;
+  }
+
+  return value;
 }
 
 function deleteCookie(cname: string) {
-    const d = new Date()
-    d.setTime(0)
-    const expires = d.toUTCString()
-    const cookie = `${cname}=; expires=${expires}; path=/;`
-    document.cookie = cookie
+  const d = new Date();
+  d.setTime(0);
+  const expires = d.toUTCString();
+  const cookie = `${cname}=; expires=${expires}; path=/;`;
+  document.cookie = cookie;
 }
 
-export { setCookie, getCookie, deleteCookie }
+export { setCookie, getCookie, deleteCookie };
